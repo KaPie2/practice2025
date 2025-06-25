f8a6a845bb964e9afb247d8e8d7c08563a3c96cc
Kate_Pirogova <pirogovakata7@gmail.com>
2025-06-25 09:50:03 +0600

    feat: add task02 projects structure

---
+++ task02/StudentService.cs
@@ -0,0 +1,35 @@
+﻿namespace task02;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+    public List<int> Grades { get; set; }
+}
+
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    // 1. Возвращает студентов указанного факультета
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+    =>  реализация;
+
+
+    // 2. Возвращает студентов со средним баллом >= minAverageGrade
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade) => реализация;
+
+    // 3. Возвращает студентов, отсортированных по имени (A-Z)
+    public IEnumerable<Student> GetStudentsOrderedByName()
+        => реализация;
+
+    // 4. Группировка по факультету
+    public ILookup<string, Student> GroupStudentsByFaculty()
+        => реализация;
+
+    // 5. Находит факультет с максимальным средним баллом
+    public string GetFacultyWithHighestAverageGrade()
+        => реализация;
+}
