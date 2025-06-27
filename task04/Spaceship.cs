093d0ebd211939dfd4582976dcfd71534f9a601e
Kate_Pirogova <pirogovakata7@gmail.com>
2025-06-27 21:34:23 +0600

    feat: implement spaceship battle system with interfaces and polymorphism

---
+++ task04/Spaceship.cs
@@ -0,0 +1,50 @@
+﻿namespace task04;
+
+public interface ISpaceship
+{
+    void MoveForward();
+    void Rotate(int angle);
+    void Fire();
+    int Speed { get; }
+    int FirePower { get; }
+}
+
+public class Cruiser : ISpaceship
+{
+    public int Speed { get; } = 50;
+    public int FirePower { get; } = 100;
+    public void MoveForward()
+    {
+        Console.WriteLine($"Крейсер движется вперед со скоростью {Speed}!");
+    }
+
+    public void Rotate(int angle)
+    {
+        Console.WriteLine($"Крейсер совершил поворот на {angle} градусов");
+    }
+
+    public void Fire()
+    {
+        Console.WriteLine($"БА-БАХ! Нанесен урон {FirePower} единиц");
+    }
+}
+
+public class Fighter : ISpaceship
+{
+    public int Speed { get; } = 100;
+    public int FirePower { get; } = 50;
+    public void MoveForward()
+    {
+        Console.WriteLine($"Истребитель движется вперед со скоростью {Speed}!");
+    }
+
+    public void Rotate(int angle)
+    {
+        Console.WriteLine($"Истребитель совершил поворот на {angle} градусов");
+    }
+
+    public void Fire()
+    {
+        Console.WriteLine($"ТЫЩЬ! Нанесен урон {FirePower} единиц");
+    }
+}
---
+++ task04tests/SpaceshipTests.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+using Moq;
+using task04;
+
+namespace task04tests;
+
+public class SpaceshipTests
+{
+    [Fact]
+    public void Cruiser_ShouldHaveCorrectStats()
+    {
+        ISpaceship cruiser = new Cruiser();
+        Assert.Equal(50, cruiser.Speed);
+        Assert.Equal(100, cruiser.FirePower);
+    }
+
+    [Fact]
+    public void Fighter_ShouldBeFasterThanCruiser()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        Assert.True(fighter.Speed > cruiser.Speed);
+    }
+
+    [Fact]
+    public void Fighter_ShouldHaveLessFirePowerThanCruise()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        Assert.True(fighter.FirePower < cruiser.FirePower);
+    }
+
+    [Fact]
+    public void Ships_ShouldMoveWithDifferentSpeed()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        fighter.MoveForward();
+        cruiser.MoveForward();
+    }
+
+    [Fact]
+    public void Rotation_ShouldWork()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        fighter.Rotate(90);
+        cruiser.Rotate(180);
+    }
+
+    [Fact]
+    public void Firing_ShouldWork()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        fighter.Fire();
+        cruiser.Fire();
+    }
+}
