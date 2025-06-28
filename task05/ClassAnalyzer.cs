c5c0a0e9610208d3e55460f598aee3c41b2d2af7
Kate_Pirogova <pirogovakata7@gmail.com>
2025-06-28 09:10:06 +0600

    feat: add ClassAnalyzer implementation template

---
+++ task05/ClassAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace task05;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods() { }
+    public IEnumerable<string> GetMethodParams(string methodname) { }
+    public IEnumerable<string> GetAllFields() { }
+    public IEnumerable<string> GetProperties() { }
+    public bool HasAttribute<T>() where T : Attribute { }
+}
