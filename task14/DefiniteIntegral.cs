f3090cbd0b3558ba60157937365fa4f6882eb33b
Kate_Pirogova <pirogovakata7@gmail.com>
2025-07-10 16:43:38 +0600

    feat: implement parallel definite integral computation using trapezoidal rule

---
+++ task14/DefiniteIntegral.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        double totalResult = 0.0;
+        int cntSteps = (int)Math.Ceiling((b - a) / step);
+        double lengthPerThread = (b - a) / threadsNumber;
+        Barrier barrier = new Barrier(threadsNumber + 1);
+
+        for (int i = 0; i < threadsNumber; i++)
+        {
+            double start = a + i * lengthPerThread;
+            double end = (i == threadsNumber - 1) ? b : start + lengthPerThread;
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                double sumOfAreas = 0.0;
+                for (double xStart = start; xStart < end; xStart += step)
+                {
+                    double xEnd = Math.Min(xStart + step, end);
+                    sumOfAreas += (function(xStart) + function(xEnd)) / 2 * (xEnd - xStart);
+                }
+
+                Interlocked.Exchange(ref totalResult, totalResult + sumOfAreas);
+                barrier.SignalAndWait();
+            });
+        }
+        barrier.SignalAndWait();
+        return totalResult;
+    }
+}
---
+++ task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using task14;
+
+namespace task14tests;
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void TestLinearFunction()
+    {
+        var X = (double x) => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void TestSinFunction()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void TestLinearPositive()
+    {
+        var X = (double x) => x;
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
