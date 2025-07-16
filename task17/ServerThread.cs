f24a3f56731d9eec59dc0ccea45f58445c385f28
Kate_Pirogova <pirogovakata7@gmail.com>
2025-07-16 09:39:46 +0600

    feat: implement ServerThread with HardStop and SoftStop commands

---
+++ task17/ServerThread.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace task17;
+
+public class ServerThread
+{
+    private readonly ConcurrentQueue<ICommand> _queue = new ConcurrentQueue<ICommand>();
+    private bool _running = false;
+    private bool _softStopRequested = false;
+    private Thread? _thread;
+
+    public void Start()
+    {
+        _running = true;
+        _softStopRequested = false;
+        _thread = new Thread(Run);
+        _thread.Start();
+    }
+
+    private void Run()
+    {
+        while (_running)
+        {
+            if (_queue.TryDequeue(out ICommand? cmd))
+            {
+                cmd.Execute();
+            }
+            else
+            {
+                if (_softStopRequested)
+                    _running = false;
+                else
+                    Thread.Sleep(50);
+            }
+        }
+    }
+
+    public void EnqueueCommand(ICommand command)
+    {
+        _queue.Enqueue(command);
+    }
+
+    public void RequestHardStop()
+    {
+        _running = false;
+    }
+
+    public void RequestSoftStop()
+    {
+        _softStopRequested = true;
+    }
+
+    public Thread? InternalThread => _thread;
+}
+
+public class HardStopCommand : ICommand
+{
+    private ServerThread _serverThread;
+    public HardStopCommand(ServerThread serverThread)
+    {
+        _serverThread = serverThread;
+    }
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread != _serverThread.InternalThread)
+            throw new InvalidOperationException("HardStop должна выполняться в потоке обработки команд!");
+        _serverThread.RequestHardStop();
+    }
+}
+
+public class SoftStopCommand : ICommand
+{
+    private ServerThread _serverThread;
+    public SoftStopCommand(ServerThread serverThread)
+    {
+        _serverThread = serverThread;
+    }
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread != _serverThread.InternalThread)
+            throw new InvalidOperationException("SoftStop должна выполняться в потоке обработки команд!");
+        _serverThread.RequestSoftStop();
+    }
+}
---
+++ task17tests/ServerThreadTests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+using task17;
+
+namespace task17tests;
+
+public class ServerThreadTests
+{
+    public class TestCommand : ICommand
+    {
+        public void Execute() { }
+    }
+
+    private bool IsThreadRunning(Thread? thread) => thread != null && thread.IsAlive;
+
+    [Fact]
+    public void HardStopCommand_StopsThreadImmediately()
+    {
+        var server = new ServerThread();
+        server.Start();
+
+        Thread.Sleep(100);
+
+        server.EnqueueCommand(new HardStopCommand(server));
+
+        var internalThread = server.InternalThread;
+        internalThread?.Join(1000);
+
+        Assert.False(IsThreadRunning(internalThread));
+    }
+
+    [Fact]
+    public void SoftStopCommand_StopsThreadAfterAllCommands()
+    {
+        var server = new ServerThread();
+        server.Start();
+
+        server.EnqueueCommand(new TestCommand());
+
+        server.EnqueueCommand(new SoftStopCommand(server));
+
+        var internalThread = server.InternalThread;
+        internalThread?.Join(1000);
+
+        Assert.False(IsThreadRunning(internalThread));
+    }
+
+    [Fact]
+    public void HardStopCommand_ThrowsIfCalledOutsideCorrectThread()
+    {
+        var server = new ServerThread();
+        server.Start();
+
+        var hardStopCmd = new HardStopCommand(server);
+
+        var ex = Assert.Throws<InvalidOperationException>(() => hardStopCmd.Execute());
+        Assert.Contains("HardStop должна выполняться", ex.Message);
+
+        server.RequestHardStop();
+        var internalThread = server.InternalThread;
+        internalThread?.Join(1000);
+    }
+
+    [Fact]
+    public void SoftStopCommand_ThrowsIfCalledOutsideCorrectThread()
+    {
+        var server = new ServerThread();
+        server.Start();
+
+        var softStopCmd = new SoftStopCommand(server);
+
+        var ex = Assert.Throws<InvalidOperationException>(() => softStopCmd.Execute());
+        Assert.Contains("SoftStop должна выполняться", ex.Message);
+
+        server.RequestHardStop();
+        var internalThread = server.InternalThread;
+        internalThread?.Join(1000);
+    }
+}
