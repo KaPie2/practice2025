240f4a51cb3b7e2fb61d84f073ad8cdf6e1498ea
Kate_Pirogova <pirogovakata7@gmail.com>
2025-07-17 10:37:28 +0600

    feat: implement multi-execution command pattern with scheduler integration

--- task17/ICommand.cs
+++ task17/ICommand.cs
@@ -1,16 +1,34 @@
-﻿namespace task17;
+﻿using task18;
+
+namespace task17;
 
 public interface ICommand
 {
     void Execute();
 }
 
-public class TestCommand(int id) : ICommand
+public class TestCommand: ICommand
 {
-    int counter = 0;
+    private readonly int id;
+    private readonly IScheduler scheduler;
+    private readonly int maxCalls;
+    private int counter = 0;
 
+    public TestCommand(int id, IScheduler scheduler, int maxCalls)
+    {
+        this.id = id;
+        this.scheduler = scheduler;
+        this.maxCalls = maxCalls;
+    }
+
+    public int ExecuteCount => counter;
+    
     public void Execute()
     {
         Console.WriteLine($"Поток {id} вызов {++counter}");
+        if (counter < maxCalls)
+        {
+            scheduler.Add(this);
+        }
     }
 }
