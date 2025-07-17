d299cecfc6534013a2d6de206b4d3648baf5966b
Kate_Pirogova <pirogovakata7@gmail.com>
2025-07-17 10:12:08 +0600

    feat: add task19 infrastructure

--- task17/ICommand.cs
+++ task17/ICommand.cs
@@ -4,3 +4,13 @@ public interface ICommand
 {
     void Execute();
 }
+
+public class TestCommand(int id) : ICommand
+{
+    int counter = 0;
+
+    public void Execute()
+    {
+        Console.WriteLine($"Поток {id} вызов {++counter}");
+    }
+}
---
+++ task19tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace task19tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
