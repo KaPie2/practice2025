548d1c72997f397573ac32358db3b9a1d209ee61
Kate_Pirogova <pirogovakata7@gmail.com>
2025-07-18 11:34:51 +0600

    feat: add command execution report and visualization

--- task17/ICommand.cs
+++ task17/ICommand.cs
@@ -1,4 +1,5 @@
 ﻿using task18;
+using System.Diagnostics;
 
 namespace task17;
 
@@ -7,12 +8,13 @@ public interface ICommand
     void Execute();
 }
 
-public class TestCommand: ICommand
+public class TestCommand : ICommand
 {
     private readonly int id;
     private readonly IScheduler scheduler;
     private readonly int maxCalls;
     private int counter = 0;
+    private Stopwatch sw = new Stopwatch();
 
     public TestCommand(int id, IScheduler scheduler, int maxCalls)
     {
@@ -21,14 +23,28 @@ public class TestCommand: ICommand
         this.maxCalls = maxCalls;
     }
 
+    public int Id => id;
+
     public int ExecuteCount => counter;
-    
+
+    public long ElapsedMs => sw.ElapsedMilliseconds;
+
     public void Execute()
     {
+        if (counter == 0)
+            sw.Start();
+
         Console.WriteLine($"Поток {id} вызов {++counter}");
+
+        Thread.Sleep(new Random().Next(1, 10));
+
         if (counter < maxCalls)
         {
             scheduler.Add(this);
+        }
+        else
+        {
+            sw.Stop();
         }
     }
 }
---
+++ task19/Program.cs
@@ -0,0 +1,58 @@
+﻿using ScottPlot;
+using task17;
+using task18;
+
+namespace task19;
+
+class Program
+{
+    static void Main()
+    {
+        int commandCount = 5;
+        int callsPerCommand = 3;
+
+        var scheduler = new RoundRobinScheduler();
+        var server = new ServerThread(scheduler);
+
+        var commands = new List<TestCommand>();
+        for (int i = 1; i <= commandCount; i++)
+        {
+            var cmd = new TestCommand(i, scheduler, callsPerCommand);
+            scheduler.Add(cmd);
+            commands.Add(cmd);
+        }
+
+        server.Start();
+
+        while (commands.Any(c => c.ExecuteCount < callsPerCommand))
+            Thread.Sleep(10);
+
+        server.EnqueueCommand(new HardStopCommand(server));
+        server.InternalThread?.Join();
+
+        double[] realTimes = commands.Select(cmd => (double)cmd.ElapsedMs).ToArray();
+        int[] commandNumbers = { 1, 2, 3, 4, 5 };
+
+        var plt = new ScottPlot.Plot();
+        plt.Title("Выполнение TestCommand");
+        plt.XLabel("Время (мс)");
+        plt.YLabel("Количество команд");
+        plt.Add.Scatter(realTimes, commandNumbers.Select(x=>(double)x).ToArray());
+
+        plt.SavePng(@"C:\Users\pirog\practice2025\command_execution_plot.png", 600, 300);
+
+        double totalExecutionTime = realTimes.Sum();
+        double averageTimePerCommand = realTimes.Average();
+        string report =
+            $"Отчет выполнения команд:\n" +
+            $"Количество команд: {commandCount}\n" +
+            $"Вызовов каждой команды: {callsPerCommand}\n" +
+            $"Общее количество вызовов Execute: {commandCount * callsPerCommand}\n" +
+            $"Тип остановки: HardStop\n\n" +
+            $"Максимальное время выполнения: {realTimes.Max()} мс\n" +
+            $"Минимальное время выполнения: {realTimes.Min()} мс\n" +
+            $"Среднее время на команду: {averageTimePerCommand:F2} мс\n" +
+            $"Общее время выполнения всех команд: {totalExecutionTime} мс\n";
+        File.WriteAllText(@"C:\Users\pirog\practice2025\command_report.txt", report);
+    }
+}
